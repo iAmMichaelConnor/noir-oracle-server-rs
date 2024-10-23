a280cbf7d24eb2c8810c995cd6c0590fff65be93
iAmMichaelConnor <mike@aztecprotocol.com>
2024-10-23 21:50:05 +0000

    handle [Field; N] oracle calls

---
+++ src/handlers.rs
@@ -0,0 +1,115 @@
+use ark_bn254::Fr;
+use ark_ff::Field;
+use num_bigint::BigUint; // It's pretty disgusting that I've introduced this library, just to convert to/from radix 16. I wonder if there's a better way with arkworks?
+use num_traits::Num; // It's pretty disgusting that I've introduced this library, just to convert to/from radix 16. I wonder if there's a better way with arkworks?
+use serde_json::{json, Value};
+// use std::str::FromStr;
+
+use crate::foreign_call::ForeignCallParam;
+
+fn sqrt(x: Fr) -> Option<Fr> {
+    let sqrt: Option<Fr> = if x.legendre().is_qr() {
+        let sqrt = x.sqrt().unwrap();
+        assert_eq!(sqrt.square(), x);
+
+        Some(sqrt)
+    } else {
+        assert_eq!(x.sqrt(), None);
+
+        None
+    };
+
+    if sqrt == None {
+        // I can't be bothered figuring out how to serialise an `Option::None` back to Noir-land, so I'm panicking in this case, instead.
+        panic!("NO SQUARE ROOT EXISTS");
+    }
+
+    sqrt
+}
+
+/**** THERE'S A LOT OF BOILERPLATE INSIDE THESE "HANDLERS", THAT WE CAN PROBABLY PUT INTO COMMON HELPER FUNCTIONS ****/
+
+/** Note: I _think_ the type `Vec<ForeignCallParam<String>>` will be generically applicable to _any_ oracle call arguments, so I've made _all_ handlers receive this type. */
+pub(crate) fn handle_get_sqrt(inputs: &Vec<ForeignCallParam<String>>) -> Value {
+    /**** EXTRACT INPUT STRING(S) ****/
+    println!("inputs: {:?}", inputs);
+    assert!(inputs.len() == 1);
+
+    let input_param = &inputs[0];
+
+    let input_string = match input_param {
+        ForeignCallParam::Single(value) => value.trim_start_matches('0'), // Trimming leading zeroes turned out to be very important, otherwise `from_str` on the next line was erroring!
+        ForeignCallParam::Array(_) => panic!("Expected single value, found array"),
+    };
+
+    println!("input_string: {:?}", input_string);
+
+    /**** CONVERT INPUT STRING(S) TO MEANINGFUL TYPE(S) ****/
+    let x_big_uint: BigUint = BigUint::from_str_radix(input_string, 16).unwrap();
+    let x: Fr = x_big_uint.into();
+    // let x: Fr = Fr::from_str(input_string).unwrap(); // This was incorrectly assuming the input_string to be decimal.
+    println!("x: {:?}", x);
+
+    /**** OPERATE ****/
+    let sqrt = sqrt(x);
+    println!("Computed sqrt: {:?}", sqrt);
+
+    /**** ENSURE HEX ****/
+    let as_big_uint: BigUint = sqrt.unwrap().into();
+    let as_hex_str = as_big_uint.to_str_radix(16);
+
+    /**** FORMAT RESULT FOR NOIR CONSUMPTION, AND CONVERT RESULT TO JSON `Value` TYPE ****/
+    //** Note: I'm converting to `Value` within these "handler" functions, instead of within the main callback (the callback inside run_server --> module.register_method --> resolve_foreign_call), because the return types can be subtly different: Vec<String>, or Vec<Vec<String>>, or maybe some more-complex arrangement of Strings and Vec<Strings>. It felt easiest to have the "hander" functions figure out how to serialise their return data. */
+    let return_values = vec![as_hex_str];
+    println!("return values: {:?}", return_values);
+
+    let json_response = json!({"values" : return_values});
+    println!("json_response: {:?}", json_response);
+    json_response
+}
+
+pub(crate) fn handle_get_sqrts(inputs: &Vec<ForeignCallParam<String>>) -> Value {
+    /**** EXTRACT INPUT STRING(S) ****/
+    println!("inputs: {:?}", inputs);
+
+    let input_param = &inputs[0];
+    let input_strings: Vec<&str> = match input_param {
+        ForeignCallParam::Single(_value) => panic!("Expected array, found single value"),
+        ForeignCallParam::Array(values) => values
+            .into_iter()
+            .map(|v| v.trim_start_matches('0'))
+            .collect(),
+    };
+
+    println!("input_strings: {:?}", input_strings);
+
+    let mut sqrts: Vec<String> = vec![];
+
+    for input_string in input_strings {
+        /**** CONVERT INPUT STRING(S) TO MEANINGFUL TYPE(S) ****/
+        println!("input_string: {:?}", input_string);
+
+        let x_big_uint: BigUint = BigUint::from_str_radix(input_string, 16).unwrap();
+        let x: Fr = x_big_uint.into();
+        // let x: Fr = Fr::from_str(input_string).unwrap(); // This was incorrectly assuming the input_string to be decimal.
+        println!("x: {:?}", x);
+
+        /**** OPERATE ****/
+        let sqrt = sqrt(x);
+        println!("Computed sqrt: {:?}", sqrt);
+
+        /**** ENSURE HEX ****/
+        let as_big_uint: BigUint = sqrt.unwrap().into();
+        let as_hex_str = as_big_uint.to_str_radix(16);
+
+        sqrts.push(as_hex_str);
+    }
+
+    /**** FORMAT RESULT FOR NOIR CONSUMPTION, AND CONVERT RESULT TO JSON `Value` TYPE ****/
+    let return_values = vec![sqrts];
+    println!("return values: {:?}", return_values);
+
+    let json_response = json!({"values" : return_values});
+    println!("json_response: {:?}", json_response);
+    json_response
+}
--- src/main.rs
+++ src/main.rs
@@ -24,26 +24,20 @@
 // IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-use std::net::SocketAddr;
-use std::time::Duration;
-
-use hyper::body::Bytes;
-use jsonrpsee::core::client::ClientT;
-use jsonrpsee::http_client::HttpClient;
-use jsonrpsee::rpc_params;
 use jsonrpsee::server::{RpcModule, Server};
-use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
-use tower_http::LatencyUnit;
+use std::net::SocketAddr;
 use tracing_subscriber::util::SubscriberInitExt;
 
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Map, Value};
+use serde::Deserialize;
+use serde_json::{json, Value};
 
-use ark_bn254::Fr;
-use ark_ff::{Field, PrimeField};
-use num_bigint::{BigUint, ToBigUint};
-use std::str::FromStr;
+mod foreign_call;
+mod handlers;
 
+use crate::foreign_call::ForeignCallParam;
+use crate::handlers::{handle_get_sqrt, handle_get_sqrts};
+
+// SPIN UP THE SERVER
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let filter = tracing_subscriber::EnvFilter::try_from_default_env()?
@@ -53,32 +47,7 @@ async fn main() -> anyhow::Result<()> {
         .finish()
         .try_init()?;
 
-    let server_addr = run_server().await?;
-
-    // MAYBE WE DON'T NEED ANY OF THIS STUFF BELOW!??
-    let url = format!("http://{}", server_addr);
-
-    let middleware = tower::ServiceBuilder::new()
-	.layer(
-		TraceLayer::new_for_http()
-			.on_request(
-				|request: &hyper::Request<_>, _span: &tracing::Span| tracing::info!(request = ?request, "on_request"),
-			)
-			.on_body_chunk(|chunk: &Bytes, latency: Duration, _: &tracing::Span| {
-				tracing::info!(size_bytes = chunk.len(), latency = ?latency, "sending body chunk")
-			})
-			.make_span_with(DefaultMakeSpan::new().include_headers(true))
-			.on_response(DefaultOnResponse::new().include_headers(true).latency_unit(LatencyUnit::Micros)),
-	);
-
-    let client = HttpClient::builder()
-        .set_http_middleware(middleware)
-        .build(url)?;
-
-    // Try a request:
-    let params = rpc_params![1_u64, 2, 3];
-    let response: Result<String, _> = client.request("say_hello", params).await;
-    tracing::info!("r: {:?}", response);
+    let _server_addr = run_server().await?;
 
     Ok(())
 }
@@ -91,54 +60,17 @@ fn print_type<T>(_: &T) {
 struct RequestData {
     session_id: u64,
     function: String,
-    inputs: Vec<String>,
+    inputs: Vec<ForeignCallParam<String>>,
     root_path: String,
     package_name: String,
 }
+
 #[derive(Debug, Deserialize)]
 struct Requests(Vec<RequestData>); // Wrap it in a struct to handle the array
 
-fn handle_get_sqrt(inputs: &Vec<String>) -> String {
-    println!("inputs: {:?}", inputs[0]);
-
-    let inputs_str = inputs[0].as_str().trim_start_matches('0'); // Trimming leading zeroes turned out to be very important, otherwise `from_str` on the next line was erroring!
-    println!("inputs_str: {:?}", inputs_str);
-
-    let x: Fr = Fr::from_str(inputs_str).unwrap();
-
-    println!("x: {:?}", x);
-
-    // SQRT CODE COPIED FROM ARKWORKS README:
-    // We can check if a field element is a square by computing its Legendre symbol...
-    let sqrt = if x.legendre().is_qr() {
-        // ... and if it is, we can compute its square root.
-        let sqrt = x.sqrt().unwrap();
-        assert_eq!(sqrt.square(), x);
-
-        Some(sqrt)
-    } else {
-        // Otherwise, we can check that the square root is `None`.
-        assert_eq!(x.sqrt(), None);
-
-        None
-    };
-
-    println!("Computed sqrt: {:?}", sqrt);
-
-    if sqrt == None {
-        // I can't be bothered figuring out how to serialise an `Option::None`, so I'm panicking in this case, instead.
-        panic!("division by zero");
-    }
-
-    // sqrt.unwrap().into_bigint().to_string()
-    let as_big_uint: BigUint = sqrt.unwrap().into();
-    let as_hex_str = as_big_uint.to_str_radix(16);
-    as_hex_str
-}
-
-fn handle_unknown_function(input: &RequestData) -> String {
+pub(crate) fn handle_unknown_function(_input: &RequestData) -> Value {
     println!("oops");
-    String::from("oops")
+    json!(vec![String::from("oops")])
 }
 
 async fn run_server() -> anyhow::Result<SocketAddr> {
@@ -147,40 +79,35 @@ async fn run_server() -> anyhow::Result<SocketAddr> {
         .await?;
     let mut module = RpcModule::new(());
 
-    module.register_method("say_hello", |_, _, _| "lo")?;
+    module.register_method("say_hello", |_, _, _| "hello, world")?;
 
     module.register_method("resolve_foreign_call", |params, _, _| {
+        println!("\n\nNEW REQUEST!!!");
         print_type(&params);
         println!("params{:?}", params);
 
-        let response: String = if let Some(json_string) = params.as_str() {
-            // Deserialize the JSON string into your struct
+        let response: Value = if let Some(json_string) = params.as_str() {
+            // Deserialize the JSON string into the Requests struct:
             let requests: Requests =
                 serde_json::from_str(&json_string).expect("Failed to parse JSON");
 
             let request = &requests.0[0];
 
-            let result: String = match request.function.as_str() {
-                "getSqrt" => handle_get_sqrt(&request.inputs),
+            println!("Request function{:?}", request.function);
+
+            let result: Value = match request.function.as_str() {
+                "get_sqrt" => handle_get_sqrt(&request.inputs), // the inputs to this are effectively a Vec<String>
+                "get_sqrts" => handle_get_sqrts(&request.inputs), // the inputs to this are effectively a Vec<Vec<String>>
                 _ => handle_unknown_function(&request),
             };
-            println!("{:?}", request.function);
-            println!("result: {:?}", result);
+
             result
         } else {
             println!("No parameters provided");
-            String::from("Bad query")
+            json!(vec![String::from("Bad query")])
         };
 
-        println!("response: {:?}", response);
-
-        // HELP! THIS IS WHAT WE'RE RETURNING.
-        // The response should be: 21888242871839275222246405745257275088548364400416034343698204186575808495615 = 0x30644E72E131A029B85045B68181585D2833E84879B9709143E1F593EFFFFFFF
-        // ... but we're getting 0x1141608e3876c1a5434c1a8094cba4e340aad219cffec5f5785d715858443db9 when printing it inside the noir program.
-        let response_j = json!({"values" : vec!(response)});
-
-        println!("response_j: {:?}", response_j);
-        response_j
+        response
     })?;
 
     let addr = server.local_addr()?;
