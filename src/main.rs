3af4cd20820031b8ec483b6d172378b8df817cae
iAmMichaelConnor <mike@aztecprotocol.com>
2024-10-23 09:42:12 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,89 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+//
+// Permission is hereby granted, free of charge, to any
+// person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the
+// Software without restriction, including without
+// limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+use std::net::SocketAddr;
+use std::time::Duration;
+
+use hyper::body::Bytes;
+use jsonrpsee::core::client::ClientT;
+use jsonrpsee::http_client::HttpClient;
+use jsonrpsee::rpc_params;
+use jsonrpsee::server::{RpcModule, Server};
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tower_http::LatencyUnit;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let filter = tracing_subscriber::EnvFilter::try_from_default_env()?
+        .add_directive("jsonrpsee[method_call{name = \"say_hello\"}]=trace".parse()?);
+    tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter(filter)
+        .finish()
+        .try_init()?;
+
+    let server_addr = run_server().await?;
+    let url = format!("http://{}", server_addr);
+
+    let middleware = tower::ServiceBuilder::new()
+	.layer(
+		TraceLayer::new_for_http()
+			.on_request(
+				|request: &hyper::Request<_>, _span: &tracing::Span| tracing::info!(request = ?request, "on_request"),
+			)
+			.on_body_chunk(|chunk: &Bytes, latency: Duration, _: &tracing::Span| {
+				tracing::info!(size_bytes = chunk.len(), latency = ?latency, "sending body chunk")
+			})
+			.make_span_with(DefaultMakeSpan::new().include_headers(true))
+			.on_response(DefaultOnResponse::new().include_headers(true).latency_unit(LatencyUnit::Micros)),
+	);
+
+    let client = HttpClient::builder()
+        .set_http_middleware(middleware)
+        .build(url)?;
+    let params = rpc_params![1_u64, 2, 3];
+    let response: Result<String, _> = client.request("say_hello", params).await;
+    tracing::info!("r: {:?}", response);
+
+    Ok(())
+}
+
+async fn run_server() -> anyhow::Result<SocketAddr> {
+    let server = Server::builder()
+        .build("127.0.0.1:0".parse::<SocketAddr>()?)
+        .await?;
+    let mut module = RpcModule::new(());
+    module.register_method("say_hello", |_, _, _| "lo")?;
+
+    let addr = server.local_addr()?;
+    let handle = server.start(module);
+
+    // In this example we don't care about doing shutdown so let's it run forever.
+    // You may use the `ServerHandle` to shut it down or manage it yourself.
+    tokio::spawn(handle.stopped());
+
+    Ok(addr)
+}
